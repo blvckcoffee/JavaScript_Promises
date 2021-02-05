c535a07f99786639b6ab408732e5e69b305ba013
blvckcoffee <joe.johnson.dev@gmail.com>
2021-02-05 11:03:07 -0600

    completed javascript promises exercise

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
-console.log("Hello World!\n==========\n");
+console.log('Hello World!\n==========\n');
 
 // Exercise 1 Section
-console.log("EXERCISE 1:\n==========\n");
+console.log('EXERCISE 1:\n==========\n');
+
+let watching = new Promise((resolve, reject) => {
+  let userWatchingLiveStream = false;
+
+  if (userWatchingLiveStream) {
+    resolve('Thumbs up and Subscribe!');
+  } else {
+    reject('User left.');
+  }
+});
+
+watching
+  .then((message) => console.log(message))
+  .catch((error) => console.log(error));
